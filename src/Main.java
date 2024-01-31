967db28b25d0af373dc4131779fd6bd39799ed33
Tapiiaa_ <ptapilob@myuax.com>
2024-01-31 13:52:14 +0100

    tecnicas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+      Prenda[]almacen= new Prenda[3];
+      for(int i=0; i<almacen.length;i++) {
+          System.out.println("Ingrese el color de la camiseta");
+          String color = sc.nextLine();
+
+          Prenda camisetauno = new Prenda(color);
+          almacen[i] = camisetauno;
+
+          System.out.println("Almacen:");
+          for (i = 0; i < almacen.length; i++){
+              System.out.println("Camiseta:"+almacen[i]);
+      }
+      sc.close();
+      }
+
+
+
     }
 
 
---
+++ src/Prenda.java
@@ -0,0 +1,12 @@
+public class Prenda {
+private String color;
+
+public Prenda(String color){
+    this.color=color;
+    }
+
+    @Override
+    public String toString() {
+        return "Color:"+color;
+    }
+}
